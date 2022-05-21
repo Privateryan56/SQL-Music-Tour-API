ac5a66cf7e6bde68a380151ec0de246246f19ae6
Austin bambino <link9215@gmail.com>
2022-05-21 13:46:27 -0400

    like mungoose

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+  
 
 // ROOT
 app.get('/', (req, res) => {
@@ -14,6 +21,8 @@ app.get('/', (req, res) => {
     })
 })
 
+
+  
 // LISTEN
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
